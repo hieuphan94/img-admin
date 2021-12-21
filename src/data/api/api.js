f546db685a9779a7f229b5366e81aff063123143
hieuphan94 <phantronghieu1@gmail.com>
2021-12-21 19:00:30 +0700

    Fix API URL HTTPS

--- src/data/api/api.js
+++ src/data/api/api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 require("dotenv").config();
 
 export default axios.create({
-  baseURL: "http://api.imagetravel.vn/api",
+  baseURL: "https://api.imagetravel.vn/api",
   responseType: "json",
 });
